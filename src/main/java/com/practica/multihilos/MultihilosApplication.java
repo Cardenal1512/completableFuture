9a7d52501eb72ac5a1312d79321cad5e1ddb1a8e
Adrián Fernández Cardenal <afcardenal27@gmail.com>
2025-06-22 11:04:43 +0200

    feat: initial project structure

---
+++ src/main/java/com/practica/multihilos/MultihilosApplication.java
@@ -0,0 +1,13 @@
+package com.practica.multihilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultihilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultihilosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/multihilos/MultihilosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.multihilos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultihilosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
